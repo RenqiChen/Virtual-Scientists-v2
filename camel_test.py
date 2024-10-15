87a6acadf8ba25a6cc4dd4dc4743440d2526c9cc
HaibaraAI <32437984+KuKuKuKuKuKurisu@users.noreply.github.com>
2024-10-15 15:44:32 +0800

    Add files via upload

---
+++ camel_test.py
@@ -0,0 +1,46 @@
+from camel.agents import ChatAgent
+from camel.messages import BaseMessage
+from camel.models import ModelFactory
+from camel.types import ModelPlatformType
+
+ollama_model = ModelFactory.create(
+    model_platform=ModelPlatformType.OLLAMA,
+    model_type="llama3.1",
+    url="http://127.0.0.1:11434/v1",
+    model_config_dict={"temperature": 0.4},
+)
+
+assistant_sys_msg0 = BaseMessage.make_assistant_message(
+    role_name="Scientist0",
+    content="You are Scientist0, You are a helpful assistant. Your task is to answer questions from Scientist1",
+)
+agent0 = ChatAgent(assistant_sys_msg0, model=ollama_model, token_limit=4096)
+
+
+assistant_sys_msg1 = BaseMessage.make_assistant_message(
+    role_name="Scientist1",
+    content="You are Scientist1, you have questions about protein to ask Scientist0",
+)
+agent1 = ChatAgent(assistant_sys_msg1, model=ollama_model, token_limit=4096)
+
+user_msg = BaseMessage.make_user_message(
+    role_name="User", content=""
+)
+
+msg = user_msg
+for i in range(2):
+    msg = agent0.step(msg)
+    print(msg.msg.content)
+    print('+'*100)
+    msg = BaseMessage.make_assistant_message(
+        role_name="Scientist0",
+        content=msg.msg.content,
+    )
+
+    msg = agent1.step(msg)
+    print(msg.msg.content)
+    print('+'*100)
+    msg = BaseMessage.make_assistant_message(
+        role_name="Scientist1",
+        content=msg.msg.content,
+    )
